3ba28f638ac6a7b43d03de8d8b2d9e77b1a9e983
andyiac <andyiaczhang@gmail.com>
2019-07-15 17:23:05 +0800

    update

--- rebuild.sh
+++ rebuild.sh
@@ -5,6 +5,12 @@ echo "======= rebuid http://org.andyiac.com ======="
 echo "======= clean ./public/ dir ================="
 rm  ~/code/org-2-html/public/*.*
 
+echo "========cp org images to public image dir ====="
+cp ~/org/image/*.* ~/code/org-2-html/public/image/
+
+echo "=== cp static html files ==========="
+cp ~/org/html/*.* ~/code/org-2-html/public/html/
+
 
 echo "======= cp css files ================="
 cp ~/code/org-2-html/src/*.css ~/code/org-2-html/public/
@@ -14,6 +20,7 @@ cp ~/code/org-2-html/src/*.js ~/code/org-2-html/public/
 
 echo "====== cp image ====================="
 cp ~/code/org-2-html/src/*.png ~/code/org-2-html/public/
+
 
 echo "======= rebuild html ======================="
 python ~/code/org-2-html/org-to-html.py
