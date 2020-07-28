78cc484b22df060a20ee79498f03c3359147ab2b
andyiac <andyiaczhang@gmail.com>
2020-07-28 11:52:24 +0800

    update

--- rebuild.sh
+++ rebuild.sh
@@ -4,13 +4,14 @@ echo "======= rebuid http://org.andyiac.com ======="
 
 echo "======= clean ./public/ dir ================="
 rm  ~/code/org-2-html/public/*.*
+# use soft link then no need to rm theses files 
+# rm  ~/code/org-2-html/public/html/*.*
 
 echo "========cp org images to public image dir ====="
 cp ~/org/image/*.* ~/code/org-2-html/public/image/
 
 echo "=== cp static html files ==========="
 cp ~/org/html/*.* ~/code/org-2-html/public/html/
-
 
 echo "======= cp css files ================="
 cp ~/code/org-2-html/src/*.css ~/code/org-2-html/public/
